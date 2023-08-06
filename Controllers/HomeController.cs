e61b374b20f9316efc8cb8b4c3ce9f0f34262cac
Nidhi <Nidhi@cybergindia.local>
2023-08-06 22:23:40 +0530

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,7 +50,6 @@ public class HomeController : Controller
  public IActionResult Admin()
     {
         List<DevicesModel> devices = GetDevicesFromBackend();
-        
         return View(devices);
     }
     private List<DevicesModel> GetDevicesFromBackend()
@@ -61,7 +60,6 @@ public class HomeController : Controller
         }catch(Exception ex){
             Console.WriteLine(ex.InnerException);
         }
-        
         return devicesList;
     }
 //    b
@@ -89,53 +87,70 @@ public class HomeController : Controller
         }
          return View("Index");
     }
-    [HttpPost("/Home/Admin")]
+[HttpPost("/Home/Admin")]
 public IActionResult AddDevice(string deviceName)
 {
     if (string.IsNullOrEmpty(deviceName))
     {
         return BadRequest("Device name is required.");
     }
-    string userName=_httpContextAccessor.HttpContext.Session.GetString("Username");
-    Console.WriteLine("username"+userName);
+    string userName = _httpContextAccessor.HttpContext.Session.GetString("Username");
+    Console.WriteLine("username" + userName);
     var device = new DevicesModel
     {
         device_name = deviceName,
         created_by = userName,
-        // updated_by = username,
         created_at_utc = DateTime.UtcNow,
         updated_at_utc = DateTime.UtcNow
     };
     _context.Device.Add(device);
     _context.SaveChanges();
-    return Ok("Device added successfully.");
+    return Json(new { Message = "Device added successfully." });
 }
-    [HttpPost("Home/Admin")]
-    public ActionResult<string> AssignDevice(int employeeID, int deviceId)
+    [HttpPost("/Home/Admin/AssignDevice")]
+    public ActionResult<string> AssignDevice([FromBody] AssignDeviceData Data)
     {
+        try{
+            int deviceId=int.Parse(Data.device_id);
+        int employeeId=int.Parse(Data.employee_id);
         List<inventoryModel> inventoryList = _context.inventory.ToList();
-        var unassignedItems = inventoryList.Where(u => u.device_id == deviceId && u.device_state == "Not Assigned").ToList();
-        Console.WriteLine("Total unassigned items for deviceId " + deviceId + ": " + unassignedItems.Count+employeeID);
+        var unassignedItems = inventoryList.Where(u => u.device_id == deviceId  && u.Specifications==Data.Specifications &&u.device_state == "Not Assigned").ToList();
+        Console.WriteLine("Total unassigned items for deviceId " + deviceId + ": " + unassignedItems.Count+" "+employeeId);
         if (unassignedItems.Count > 0)
         {
             Random random = new Random();
             int selectedIndex = random.Next(0, unassignedItems.Count);
             inventoryModel selectedDevice = unassignedItems[selectedIndex];
-            selectedDevice.device_state = "Assigned";
-            _context.SaveChanges();
             var assignedItem = new AssignedDevicesModel
             {
-                employee_id = employeeID,
+                employee_id = employeeId,
                 inventory_id = selectedDevice.inventory_id,
                 created_at_utc = DateTime.UtcNow
             };
+              selectedDevice.device_state = "Assigned";
+              try{
+                          _context.inventory.Update(selectedDevice);
+            _context.SaveChanges();
             _context.AssignedDevices.Add(assignedItem);
             _context.SaveChanges();
-            return "assigned";
+              }catch (Exception ex){
+                Console.WriteLine("Test CHeck");
+                Console.WriteLine(ex.InnerException);
+              }
+             return "assigned";
         }
     else{
-        return "all devices are assigned";}
+        return Ok(new { Message = "all devices are assigned" });}
+    } catch(Exception ex){
+        Console.WriteLine(ex.InnerException);
     }
+        return null;
+    }
+    public class AssignDeviceData{
+    public string employee_id{get;set;}
+    public string device_id{get;set;}
+    public string Specifications{get;set;}
+}
     // public static string GenerateSecurityKey()
     // {
     //     const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -171,6 +186,89 @@ public IActionResult AddDevice(string deviceName)
         var tokenHandler = new JwtSecurityTokenHandler();
         return tokenHandler.WriteToken(token);
     }
+    //populating specs
+    [HttpGet("/Home/GetSpecifications/{deviceId}")]
+public IActionResult GetSpecifications(int deviceId)
+{
+    // Here, you should write the logic to fetch the specifications based on the selected device ID.
+    // For example:
+    // var specifications = GetSpecificationsFromBackend(deviceId);
+    // For demonstration purposes, let's assume we have a list of specifications related to the selected device ID:
+    var specifications = _context.inventory
+        .Where(s => s.device_id == deviceId)
+        .Select(s => new { Value = s.Specifications, Text = s.Specifications }).Distinct()
+        .ToList();
+    // Return the specifications as JSON
+    return Json(specifications);
+}
+//add user
+[HttpPost("/Home/Admin/AddUser")]
+public async Task<IActionResult> AddUser(UserInfoModel UserInfo)
+{
+    if (_context.UserInfo.Any(e => e.employee_id == UserInfo.employee_id))
+    {
+        return BadRequest(new { error = "User id already exists." });
+    }
+    string hashedpassword = HashPassword(UserInfo.password);
+    var user = new  UserInfoModel
+    {
+        employee_id = UserInfo.employee_id,
+        username = UserInfo.username,
+        password = hashedpassword
+    };
+    _context.UserInfo.Add(user);
+    await _context.SaveChangesAsync();
+    return Ok("User added successfully");
+}
+// [HttpGet("/Home/Admin/GetUsername")]
+// public ActionResult<UserData> GetUsername(){
+//     var userData=new UserData();
+//     string userName=_httpContextAccessor.HttpContext.Session.GetString("Username");
+//     var user=_context.UserInfo.FirstOrDefault(u=>u.username==userName);
+//     userData.username=user.username;
+//     userData.employee_id=user.employee_id;
+//     return userData;
+// }
+// public class UserData{
+//     public string username {get;set;}
+//     public int employee_id{get;set;}
+// }
+// API endpoint to add a device to the inventory
+[HttpPost("/Home/Admin/AddToInventory")]
+public IActionResult AddToInventory([FromBody] inventoryModel inventoryEntry, string specifications)
+{
+    // Retrieve the device based on the selected device_id
+    var device = _context.Device.FirstOrDefault(d => d.device_id == inventoryEntry.device_id);
+    if (device == null)
+    {
+        return NotFound("Device not found.");
+    }
+    // Set the created_at_utc and updated_at_utc properties
+    inventoryEntry.device_id = device.device_id;
+    inventoryEntry.created_at_utc = DateTime.UtcNow;
+    inventoryEntry.updated_at_utc = DateTime.UtcNow;
+    inventoryEntry.device_state="Not Assigned";
+    // If specifications are not provided or empty, set it to "NA"
+    // if (string.IsNullOrWhiteSpace(specifications))
+    // {
+    //     inventoryEntry.Specifications = "NA";
+    // }
+    // else
+    // {
+        inventoryEntry.Specifications = specifications;
+    // }
+    // Add the inventory entry to the database
+    _context.inventory.Add(inventoryEntry);
+    _context.SaveChanges();
+    return Ok();
+}
+//   private readonly Dictionary<string, int[]> dataMap = new Dictionary<string, int[]>
+//     {
+//         { "option1", new int[] { , 30, 30 } },
+//         { "option2", new int[] { 10, 50, 40 } },
+//         { "option3", new int[] { 20, 20, 60 } },
+//         { "option4", new int[] { 25, 35, 40 } }
+//     };
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Models/UserInfoModel.cs
+++ Models/UserInfoModel.cs
@@ -6,24 +6,16 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace ITGateway.Models
 {
- 
-
-
   public class UserInfoModel
 {
     [Key]
     public int id { get; set; }
-
     [StringLength(100)]
     public string username { get; set; }
-
     [StringLength(255)]
     public string password { get; set; }
-
     public int employee_id { get; set; }
-
     [ForeignKey("employee_id")]
     public virtual EmployeeModel Employee { get; set; }
 }
-
 }
